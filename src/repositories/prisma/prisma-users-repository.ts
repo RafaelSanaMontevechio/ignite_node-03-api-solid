a27450506932ae8f26065fb694519b3f4d22bbbd
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-08-14 08:34:59 -0300

    feat: add find user by oid method

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -5,9 +5,9 @@ import { prisma } from '@/lib/prisma';
 import { UsersRepository } from '../users-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
-  async findById(userId: string) {
+  async findById(id: string) {
     const user = await prisma.user.findUnique({
-      where: { id: userId },
+      where: { id },
     });
 
     return user;
